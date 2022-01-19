d8665b11f779c57dff8bc2378746734daef09f98
alexandra <alexandraschurman@gmail.com>
2022-01-19 10:52:47 -0800

    commit tree 85e9c42f52479387c0600dbf9913156a9e647601

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code goes here
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     console.log("The DOM has loaded");
+
+    
+// })
+
+
+// console.log(
+//     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+//   );
+  
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+
+    document.querySelector('p').textContent = "This is really cool!" 
+    
+})
+
+//DOM content loaded does not wait for stylesheets and images to load
